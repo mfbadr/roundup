ebc730ded2abb1320ae44ad6ba6fef24d7d9787e
Mikey Badr <mfbadr@gmail.com>
2014-10-27 13:39:26 -0500

    added stars and movement

--- public/play.js
+++ public/play.js
@@ -34,11 +34,8 @@ var playState = {
 
     //  Now let's create two ledges
     var ledge = platforms.create(400, 400, 'ground');
-
     ledge.body.immovable = true;
-
     ledge = platforms.create(-150, 150, 'ground');
-
     ledge.body.immovable = true;
 
       // The player and its settings
@@ -55,9 +52,36 @@ var playState = {
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    //spawn 10 xs
+    stars = game.add.group();
+    stars.enableBody = true;
+
+    for(var i = 0; i < 10; i++){
+      var star = stars.create(i * 70, 0, 'star');
+      star.body.gravity.y = 60;
+      star.body.bounce.y = 0.7 + Math.random() * 0.2;
+      star.body.collideWorldBounds = true;
+    };
+
+    this.timer = game.time.events.loop(1500, this.moveStars, this);
   },
 
-  //
+  moveStars: function(){
+    //call this on a timer in create
+    //will look like this:
+    //this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    stars.forEach(function(star){
+      var direction = Math.floor(Math.random() + .5);
+
+      if(direction === 1){
+        star.body.velocity.x += 100;
+      }else if(direction === 0){
+        star.body.velocity.x -= 100;
+      };
+    }, this)
+  },
 
   update: function(){
     //this is the biggest part of the game
@@ -69,12 +93,14 @@ var playState = {
     //bound player to screen
 
     //if timer reaches zero, play and animation and return to menu
+    //stars collide with platforms
+    game.physics.arcade.collide(stars, platforms)
 
     //player collision w/ platform
-     game.physics.arcade.collide(player, platforms);
-     cursors = game.input.keyboard.createCursorKeys();
+    game.physics.arcade.collide(player, platforms);
+    cursors = game.input.keyboard.createCursorKeys();
 
-     player.body.velocity.x = 0;
+    player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
     {
@@ -94,16 +120,12 @@ var playState = {
     {
         //  Stand still
         player.animations.stop();
-
         player.frame = 4;
     }
-
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && player.body.touching.down)
     {
         player.body.velocity.y = -350;
     }
-
-
   }
 };
