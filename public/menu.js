0f1d975fbf23a32712cd37b9f803485c0f24c7df
Logan Richardson <g.logan.richardson@gmail.com>
2014-10-27 11:49:53 -0500

    added menu

--- public/menu.js
+++ public/menu.js
@@ -1,10 +1,25 @@
-var menu_state = {
+var menu_state = {  
   create: function(){
-    //press any key to start goes here
-
-    //some text explaining the game
-    //display last score
-
-    // a start function to change game state to play
+    // Call the 'start' function when pressing the spacebar
+    var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    space_key.onDown.add(this.start, this); 
+    // Defining variables
+    var style = {font: "30px Arial", fill: "#ffffff"};
+    var x = game.world.width/2, y = game.world.height/2;
+    // Adding a text centered on the screen
+    var text = this.game.add.text(x, y-50, "Press space to start", style);
+    var text2 = this.game.add.text(x, y-60, "Use <- Left, ^ Up, -> Right keys to move character", style);
+    text.anchor.setTo(0.5, 0.5);
+    text2.anchor.setTo(0.5, 0.5); 
+    // If the user already played
+    if (score > 0) {
+        // Display its score
+        var score_label = this.game.add.text(x, y+50, "score: " + score, style);
+        score_label.anchor.setTo(0.5, 0.5); 
+    }
   },
+  // Start the actual game
+  start: function(){
+      this.game.state.start('play');
+  }
 };
