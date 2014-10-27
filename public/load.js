0455ddf9516058284717bb33916b805c13e96708
Dennis Stanley <stanlsytd@gmail.com>
2014-10-27 11:39:39 -0500

    load.js setup

--- public/load.js
+++ public/load.js
@@ -3,9 +3,14 @@ var load_state = {
     //load player spritesheet, x and x images
     //sounds
     //make
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   },
 
   create: function(){
     //when all assets are loaded, go to the menu state
+    this.game.state.start('menu');
   }
 };
