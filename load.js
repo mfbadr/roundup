261d80831c07680b8c49d952aaf4f7fa2ee56757
Mikey Badr <mfbadr@gmail.com>
2014-11-07 10:45:09 -0600

    fixed filepaths

--- load.js
+++ load.js
@@ -3,19 +3,19 @@ var loadState = {
     //load player spritesheet, x and x images
     //sounds
     //make
-    game.load.image('sky', '/img/sky.png');
-    game.load.image('ground', '/img/platform.png');
-    game.load.image('star', '/img/star.png');
-    game.load.image('diamond', '/img/diamond.png');
-    game.load.spritesheet('dude', '/img/dude.png', 32, 48);
+    game.load.image('sky', '/roundup/img/sky.png');
+    game.load.image('ground', '/roundup/img/platform.png');
+    game.load.image('star', '/roundup/img/star.png');
+    game.load.image('diamond', '/roundup/img/diamond.png');
+    game.load.spritesheet('dude', '/roundup/img/dude.png', 32, 48);
     //Game Sound
-    game.load.audio('game', 'assets/audio/Totta - Hero Quest - Pophousedub remix.mp3', 'assets/audio/Totta_-_Hero_Quest_-_Pophousedub_remix.ogg');
+    game.load.audio('game', '/roundup/assets/audio/Totta - Hero Quest - Pophousedub remix.mp3', '/roundup/assets/audio/Totta_-_Hero_Quest_-_Pophousedub_remix.ogg');
     //Load in the jump sound
-    game.load.audio('jump', 'assets/audio/SoundEffects/alien_death1.wav');
+    game.load.audio('jump', '/roundup/assets/audio/SoundEffects/alien_death1.wav');
     //Sound for Stars
-    game.load.audio('star', 'assets/audio/SoundEffects/key.wav');
+    game.load.audio('star', '/roundup/assets/audio/SoundEffects/key.wav');
     //Sound for Diamonds
-    game.load.audio('diamond', 'assets/audio/SoundEffects/p-ping.mp3')
+    game.load.audio('diamond', '/roundup/assets/audio/SoundEffects/p-ping.mp3')
   },
 
   create: function(){
